6cd41bc0cbaeb9f37977c2806aada2dd48959230
gbl20 <484175472@qq.com>
2020-07-22 15:24:39 +0800

    提交时间 2020/07/22 15:24

---
+++ demo/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="app">
+		<div id="nav">
+			<div class="nav-content">
+				<div class="login">
+					<span>Welcome to Your Vue.js App</span>
+					<router-link to="/">sign</router-link>
+				</div>
+				<div class="nav-router">
+					<router-link to="/">Home</router-link> |
+					<router-link to="/about">About</router-link> |
+					<router-link to="/News">News</router-link>
+				</div>
+			</div>
+
+		</div>
+		<router-view/>
+	</div>
+</template>
+
+<style>
+	@import url('assets/css/common/common.css');
+</style>
---
+++ demo/src/views/News.vue
@@ -0,0 +1,20 @@
+<template>
+	<h1>
+		{{msg}}
+	</h1>
+</template>
+
+<script>
+	export default {
+		name: "News",
+		data:function () {
+			return{
+				msg:"This is My News"
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
